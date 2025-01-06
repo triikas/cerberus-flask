d792f8a2e10488700e9308f1f9ba967030a8ae08
TriLP <triikas@magnavis.ru>
2025-01-07 00:01:34 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+from flask_mobility import Mobility
+from flask_mobility.decorators import mobile_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['post', 'get'])
+@mobile_template('home{-mobile}.html')
+def home(template):
+    # if request.method == 'POST':
+    #     msg = Message("Запрос с skyparts.ru", recipients=mails)
+    #     name = request.form.get('name')
+    #     email = request.form.get('email')
+    #     number = request.form.get('number')
+    #     comment = request.form.get('comment')
+    #     msg.body = "Имя:     {}\nПочта:   {}\nТелефон: {}\n\nКомментарий:\n\n{}".format(name, email, number, comment)
+    #     if name is not None:
+    #         mail.send(msg)
+
+    return render_template(template)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/func.js
@@ -0,0 +1,67 @@
+
+(function () {
+
+      const link = document.querySelectorAll('nav > .hover-this');
+      const cursor = document.querySelector('.cursor');
+
+      const animateit = function (e) {
+            const span = this.querySelector('span');
+            const { offsetX: x, offsetY: y } = e,
+            { offsetWidth: width, offsetHeight: height } = this,
+
+            move = 25,
+            xMove = x / width * (move * 2) - move,
+            yMove = y / height * (move * 2) - move;
+
+            span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+            if (e.type === 'mouseleave') span.style.transform = '';
+      };
+
+      const editCursor = e => {
+            const { clientX: x, clientY: y } = e;
+            cursor.style.left = x + 'px';
+            cursor.style.top = y + 'px';
+      };
+
+      link.forEach(b => b.addEventListener('mousemove', animateit));
+      link.forEach(b => b.addEventListener('mouseleave', animateit));
+      window.addEventListener('mousemove', editCursor);
+
+})();
+
+document.querySelectorAll('.hover-this').forEach(item => {
+  item.addEventListener('mouseover', () => {
+    document.querySelectorAll('.cursor').forEach(cursor => {
+      cursor.classList.add('large');
+    });
+  });
+
+  item.addEventListener('mouseout', () => {
+    document.querySelectorAll('.cursor').forEach(cursor => {
+      cursor.classList.remove('large');
+    });
+  });
+});
+
+document.querySelectorAll('.hover-this-2').forEach(item => {
+  item.addEventListener('mouseover', () => {
+    document.querySelectorAll('.cursor').forEach(cursor => {
+      cursor.classList.add('large-2');
+    });
+  });
+
+  item.addEventListener('mouseout', () => {
+    document.querySelectorAll('.cursor').forEach(cursor => {
+      cursor.classList.remove('large-2');
+    });
+  });
+});
+
+var embed = new Twitch.Embed('twitch-embed', {
+  channel: 'cerberus__esports'
+});
+
+embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
+  console.log('The video is ready');
+});
